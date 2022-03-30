c88bd81e5698f237a87e1896a9476de6d15dbcba
Carlos Alvarez del Castillo <carlosadelcs@tamu.edu>
2022-03-30 13:20:17 -0500

    Add load_factor test

--- tests/tests/load_factor.cpp
+++ tests/tests/load_factor.cpp
@@ -1,5 +1,24 @@
 #include "executable.h"
 
 TEST(load_factor) {
-    
+    Typegen t;
+    for(size_t i = 0; i < TEST_ITER; i++) {
+        using Map = UnorderedMap<std::string, double>;
+        using value_type = std::pair<std::string, double>;
+
+        size_t n_pairs = t.range(1000ul);
+
+        std::vector<value_type> pairs(n_pairs);
+        t.fill(pairs.begin(), pairs.end());
+
+        size_t sz = t.range(100ull);
+        size_t n_buckets =  next_greater_prime(sz);
+        Map map(n_buckets);
+
+        for(size_t k=0; k<n_pairs; k++) {
+            map.insert(pairs[k]);
+            ASSERT_EQ(static_cast<float>(k + 1)/static_cast<float>(n_buckets),
+                      map.load_factor());
+        }
+    }
 }
