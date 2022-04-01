709a6c821ccedc0c805657e878cb74795fc91f8f
Carlos Alvarez del Castillo <carlosadelcs@tamu.edu>
2022-03-31 22:28:47 -0500

    added iterator and bucket iterator tests

--- tests/tests/bucket_iterator.cpp
+++ tests/tests/bucket_iterator.cpp
@@ -1,5 +1,60 @@
 #include "executable.h"
 
 TEST(bucket_iterator) {
-    
+    Typegen t;
+
+    using Map = UnorderedMap<int, int>;
+    using iter = typename UnorderedMap<int, int>::local_iterator;
+    using value_type = std::pair<int, int>;
+
+    // check iterator functions
+    for(size_t i = 0; i < TEST_ITER; i++) {
+        const size_t sz = t.range(100UL);
+        const size_t num_pairs = t.range(1000UL);
+
+        Map map(sz);
+        shadow_map<int, int> shad_map(sz);
+
+        for(size_t i = 0; i<map.bucket_count(); i++) {
+            ASSERT_TRUE(map.begin(i) == map.end(i)); 
+        }
+
+        std::vector<value_type> pairs(num_pairs);
+        t.fill(pairs.begin(), pairs.end());
+
+        for(auto pair: pairs) {
+            map.insert(pair);
+            shad_map.insert(pair);
+        }
+
+        {
+            Memhook mh;
+
+            for(size_t bucket = 0; bucket < shad_map.bucket_count(); bucket++) {
+
+                ASSERT_EQ(shad_map.bucket_size(bucket), map.bucket_size(bucket));
+
+                for(iter it = map.begin(bucket); it != map.end(bucket); it++) {
+                    ASSERT_TRUE(shad_map.contained_in_bucket(it->first, bucket));
+                }
+
+                for(iter it = map.begin(bucket); it != map.end(bucket); ++it) {
+                    ASSERT_TRUE(shad_map.contained_in_bucket(it->first, bucket));
+                }
+
+                iter it_a = map.begin(bucket);
+                iter it_b = map.begin(bucket);
+
+                while(it_a != map.end(bucket)) {
+                    ASSERT_TRUE(it_a == it_b);
+                    ASSERT_TRUE(*it_a == *it_b);
+                    it_a++;
+                    it_b++;
+                }
+            }
+
+            ASSERT_EQ(0UL, mh.n_scoped_frees());
+            ASSERT_EQ(0UL, mh.n_allocs());
+        }
+    }
 }
--- tests/tests/iterator.cpp
+++ tests/tests/iterator.cpp
@@ -1,5 +1,59 @@
 #include "executable.h"
 
 TEST(iterator) {
-    
+    Typegen t;
+
+    using Map = UnorderedMap<int, int>;
+    using iter = typename UnorderedMap<int, int>::iterator;
+    using value_type = std::pair<int, int>;
+
+    // check iterator functions
+    for(size_t i = 0; i < TEST_ITER; i++) {
+        const size_t sz = t.range(100UL);
+        const size_t num_pairs = t.range(1000UL);
+
+        Map map(sz);
+        std::unordered_map<int, int> shad_map(sz);
+
+        ASSERT_TRUE(map.begin() == map.end());
+
+        std::vector<value_type> pairs(num_pairs);
+        t.fill(pairs.begin(), pairs.end());
+
+        for(auto pair: pairs) {
+            map.insert(pair);
+            shad_map.insert(pair);
+        }
+
+        {
+            Memhook mh;
+
+            ASSERT_EQ(shad_map.size(), map.size());
+
+            for(iter it = map.begin(); it != map.end(); it++) {
+                auto found = shad_map.find(it->first);
+                ASSERT_TRUE(found != shad_map.end());
+            }
+
+            for(iter it = map.begin(); it != map.end(); ++it) {
+                auto found = shad_map.find(it->first);
+                ASSERT_TRUE(found != shad_map.end());
+            }
+
+            iter it_a = map.begin();
+            iter it_b = map.begin();
+            size_t count = 0UL;
+            while(it_a != map.end()) {
+                ASSERT_TRUE(it_a == it_b);
+                ASSERT_TRUE(*it_a == *it_b);
+                it_a++;
+                it_b++;
+                count ++;
+            }
+            ASSERT_EQ(map.size(), count);
+
+            ASSERT_EQ(0UL, mh.n_scoped_frees());
+            ASSERT_EQ(0UL, mh.n_allocs());
+        }
+    }
 }
